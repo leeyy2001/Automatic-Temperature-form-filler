309f96d54a13d2a9ba8b420e02cc9c06a4fb9a43
leeyy2001 <75061170+leeyy2001@users.noreply.github.com>
2021-06-12 20:42:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import datetime
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import pyautogui
+
+day = datetime.datetime.now()
+
+#instantiates the webdriver object and sets up the window to be in incognito mode.
+option = webdriver.ChromeOptions()
+option.add_argument("-incognito")
+
+#defines the browser and option
+browser = webdriver.Chrome(executable_path=r"D:\yylee\chromedriver.exe", options=option)
+
+#Accesses the website using the link we provided it.
+browser.get("https://docs.google.com/forms/d/e/1FAIpQLSdu2cOJxJtZjN-of4ycISSwyxw3o6DwO84CwKklpJJXu3QMUQ/viewform")
+
+time.sleep(2)
+
+#Finding all the classes that we will be using in this code for it to work.
+date = browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")
+rank_name = browser.find_elements_by_class_name("quantumWizMenuPaperselectContent")
+temperature = browser.find_elements_by_class_name("quantumWizTextinputPaperinputInput")
+submit_btn = browser.find_elements_by_class_name("appsMaterialWizButtonPaperbuttonContent")
+
+#This sets up the day in dd/mm/yyyy format, and keys it into the form.
+date[0].send_keys(day.strftime("%m" + "/" + "%d" + "/" + "%Y"))
+
+#The function to select the appropriate Rank and Name selection from the respective dropdown menus.
+def dropdown_clicker(x):
+    for i in range(x):
+        pyautogui.press("down")
+    pyautogui.press("enter")
+
+#Keying in the Rank and Name as well as temperature into the respective entries.
+rank_name[0].click()
+time.sleep(1)
+dropdown_clicker(12)
+time.sleep(1)
+rank_name[14].click()
+time.sleep(1)
+dropdown_clicker(17)
+time.sleep(1)
+
+temperature[1].send_keys("36.5")
+temperature[2].send_keys("36.5")
+
+#Click on the submit button.
+submit_btn[0].click()
+
+time.sleep(5)
